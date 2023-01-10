12e32d74ce036f6342f4a098b8ae8ddc8ad2701f
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-01-10 13:47:07 +0700

    update factories user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -27,17 +26,5 @@ class UserFactory extends Factory
             'gps_code' => fake()->randomDigit(),
             'level' => fake()->randomElement(['user', 'admin']),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return static
-     */
-    public function unverified()
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
