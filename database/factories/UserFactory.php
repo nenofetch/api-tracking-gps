f1f302a7a2a07039f73d782c2e7540c71843a8fc
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-01-10 14:08:32 +0700

    update factories user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,6 +25,7 @@ class UserFactory extends Factory
             'address' => fake()->address(),
             'gps_code' => fake()->randomDigit(),
             'level' => fake()->randomElement(['user', 'admin']),
+            'remember_token' => Str::random(10),
         ];
     }
 }
