9777174d8a4098225fc742181cf3627da47e0151
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-01-10 12:44:15 +0700

    update migration tokens

--- database/migrations/2023_01_10_052438_tokens.php
+++ database/migrations/2023_01_10_052438_tokens.php
@@ -15,10 +15,11 @@ return new class extends Migration
     {
         Schema::create('tokens', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
+            $table->unsignedBigInteger('user_id');
             $table->rememberToken();
             $table->timestamp('date_expired');
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
