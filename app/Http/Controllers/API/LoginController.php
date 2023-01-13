9f0d63835c9d1b1bf9310a976a108839fe945c57
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-01-13 23:14:24 +0700

    update login controller

--- app/Http/Controllers/API/LoginController.php
+++ app/Http/Controllers/API/LoginController.php
@@ -12,6 +12,15 @@ class LoginController extends BaseController
 {
     public function login(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|min:5',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
         if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
             $user = Auth::user();
             $success['token'] = $user->createToken('TokenUser')->accessToken;
