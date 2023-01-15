a76bcc138420e1b1b3899634c5f7f85b3c188b48
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-01-15 11:40:14 +0700

    update migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->string('no_hp');
             $table->string('address')->nullable();
             $table->string('gps_code')->nullable();
-            $table->enum('level', ['user', 'admin'])->default('user');
+            // $table->enum('level', ['user', 'admin'])->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
